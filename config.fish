16e5d99928282438fe40802f11e70d41a42ae8bc
ishida <ishida@laboro.ai>
2025-06-28 06:21:46 +0900

    add git alias

--- config.fish
+++ config.fish
@@ -1,11 +1,15 @@
 # ~/.config/fish/config.fish
 
 # エイリアス（関数）
+
+alias g="git"
 alias gs="git status"
 alias gc="git commit"
 alias gcm="git commit -m"
 alias gl="git log --oneline --graph --all"
 alias ga="git add"
+alias push="git push"
+alias pull="git pull"
 
 # 色付き ls（Macの場合は gls 推奨）
 alias ls="ls --color=auto"
@@ -24,4 +28,7 @@ set -x EDITOR nvim
 
 # PATH の追加
 set -x PATH $HOME/.local/bin $PATH
+set -x PATH $HOME/.lmstudio/bin $PATH
+set -x PATH $HOME/go/bin $PATH
+set -x PATH /opt/homebrew/bin $PATH
 
